67b80dcb4826153acabbdf185782b75107822485
marcus.jarlevid@gmail.com <marcus.jarlevid@gmail.com>
2020-09-09 09:06:04 +0200

    Add project files.

---
+++ Exercise 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var keepAlive = true;
+            while (keepAlive)
+            {
+                try
+                {
+                    Console.Write("Enter assignment number (or -1 to exit):");
+                    var assignmentChoice = int.Parse(Console.ReadLine() ?? "");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    switch (assignmentChoice)
+                    {
+                        case 1:
+                            RunExerciseOne();
+                            break;
+                        case 2:
+                            RunExerciseTwo();
+                            break;
+                        case 3:
+                            //call your next assignment
+                            break;
+                        case 
+                    }
+                }
+            }
+        }
+    }
+}
