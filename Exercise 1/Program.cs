537119f2a0a196c2f9b1623f0648fdc1319bb846
marcus.jarlevid@gmail.com <marcus.jarlevid@gmail.com>
2020-09-14 14:57:41 +0200

    Exercise 1+2 done

--- Exercise 1/Program.cs
+++ Exercise 1/Program.cs
@@ -25,12 +25,69 @@ namespace Exercise_1
                             RunExerciseTwo();
                             break;
                         case 3:
-                            //call your next assignment
+                            RunExerciseThree();
                             break;
-                        case 
+                        case -1:
+                            keepAlive = false;
+                            break;
+                        default:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("That is not a valid assignment number.");
+                            break;
                     }
+                    Console.ResetColor();
+                    Console.WriteLine("Hit any key to continue.");
+                    Console.ReadKey();
+                    Console.Clear();
                 }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("That is not a valid assignment number.");
+                    Console.ResetColor();
+                }
+
             }
+
+
         }
+
+
+        private static void RunExerciseOne()
+            {
+            string firstname = "Jamarcus";
+            string lastname = "Jarule";
+
+            Console.Clear();
+
+            Console.WriteLine("You've successfully ran exercise One!");
+            //Console.Write();
+            Console.Write("Hello, " + firstname + " " +  lastname + "! I'm glad to inform you that you are the test subject of my very first assignment!"); 
+            Console.WriteLine();
+
+            }
+
+        private static void RunExerciseTwo()
+        {
+            Console.Clear();
+            Console.WriteLine("You've successfully ran exercise Two!");
+
+
+            var todaysdate = DateTime.Today;
+            string format = "dddd d, MMMM, yyyy";
+            Console.WriteLine("Todays date is " + todaysdate.ToString(format));// eget format
+
+            var tomorrowsdate = todaysdate.AddDays(1);
+            Console.WriteLine("Tomorrows date is " + tomorrowsdate.ToShortDateString());// format short string (DD/MM/YYY)
+
+            var yesterdaysdate = todaysdate.AddDays(-1);
+            Console.WriteLine("Yesterdays date was " + yesterdaysdate.ToLongDateString());// format long string (den 13 September, 2020)
+
+        }
+        private static void RunExerciseThree()
+        {
+            string firstname = AskUserFor
+        }
+
     }
 }
