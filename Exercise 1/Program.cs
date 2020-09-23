212616f1816c03d2f32067fb6d4d295ab70fe090
marcus.jarlevid@gmail.com <marcus.jarlevid@gmail.com>
2020-09-23 16:34:16 +0200

    exercise 18 done

--- Exercise 1/Program.cs
+++ Exercise 1/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace Exercise_1
 {
@@ -6,7 +9,7 @@ namespace Exercise_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to CodeMaster3000!");
 
             var keepAlive = true;
             while (keepAlive)
@@ -51,6 +54,27 @@ namespace Exercise_1
                         case 11:
                             RunExerciseEleven();
                             break;
+                        case 12:
+                            RunExerciseTwelve();
+                            break;
+                        case 13:
+                            RunExerciseThirteen();
+                            break;
+                        case 14:
+                            RunExerciseFourteen();
+                            break;
+                        case 15:
+                            RunExerciseFifteen();
+                            break;
+                        case 16:
+                            RunExerciseSixteen();
+                            break;
+                        case 17:
+                            RunExerciseSeventeen();
+                            break;
+                        case 18:
+                            RunExerciseEightteen();
+                            break;
                         case -1:
                             keepAlive = false;
                             break;
@@ -74,11 +98,16 @@ namespace Exercise_1
             }
 
 
+        }//Main end
+
+        //Template
+        private static void RunExercise()
+        {
+
         }
 
-
         private static void RunExerciseOne()
-            {
+        {
             string firstname = "Jamarcus";
             string lastname = "Jarule";
 
@@ -86,10 +115,10 @@ namespace Exercise_1
 
             Console.WriteLine("You've successfully ran exercise One!");
             //Console.Write();
-            Console.Write("Hello, " + firstname + " " +  lastname + "! I'm glad to inform you that you are the test subject of my very first assignment!"); 
+            Console.Write("Hello, " + firstname + " " + lastname + "! I'm glad to inform you that you are the test subject of my very first assignment!");
             Console.WriteLine();
 
-            }
+        }
 
         private static void RunExerciseTwo()
         {
@@ -118,20 +147,20 @@ namespace Exercise_1
 
             Console.WriteLine("Your full name is: " + fullName);
 
-        static string AskUserFor(string whatfor)
-        {
-            Console.Write("Please enter " + whatfor + ": ");
-            string userInput = Console.ReadLine();
+            static string AskUserFor(string whatfor)
+            {
+                Console.Write("Please enter " + whatfor + ": ");
+                string userInput = Console.ReadLine();
 
-            return userInput;
-        }
+                return userInput;
+            }
 
-        static string CombineFirstAndLastNameIntoFullName(string firstName, string lastName)
-        {
-            string fullName = firstName + " " + lastName;
+            static string CombineFirstAndLastNameIntoFullName(string firstName, string lastName)
+            {
+                string fullName = firstName + " " + lastName;
 
-            return fullName;
-        }
+                return fullName;
+            }
 
         }
 
@@ -140,9 +169,9 @@ namespace Exercise_1
             String str = "The quick fox Jumped Over the DOG";
 
             str = str.Remove(4, 6); // The fox Jumped Over the DOG
-            
+
             str = str.Insert(3, " brown"); // The brown fox Jumped Over the DOG
-            
+
             string dog = str.Replace("DOG", "lazy dog");//The brown fox Jumped Over the lazy dog
 
             string index1 = dog.Substring(14, 1); //index1 = "J"
@@ -151,7 +180,7 @@ namespace Exercise_1
             string final = dog.Replace(index1, "j");
             final = final.Replace(index2, "o"); //The brown fox jumped over the lazy dog
 
- 
+
             //string final = dog.Replace("Jumped Over", "jumped over");
 
             Console.WriteLine(final);
@@ -177,18 +206,18 @@ namespace Exercise_1
             int input1;
             int input2;
 
-           
+
             Console.WriteLine("Please enter a number: ");
-            input1 = TextToNumber(Console.ReadLine()); 
-            
-            
+            input1 = TextToNumber(Console.ReadLine());
+
+
             Console.WriteLine("Please enter a number: ");
             input2 = TextToNumber(Console.ReadLine());
 
 
-            if (input1 > input2) 
+            if (input1 > input2)
             {
-                
+
                 Console.WriteLine(input1 + " is the biggest number.");
                 Console.WriteLine(input2 + " is the smallest number.");
 
@@ -206,10 +235,10 @@ namespace Exercise_1
             }
             else
             {
-                
+
                 Console.WriteLine(input2 + " is the biggest number.");
                 Console.WriteLine(input1 + " is the smallest number.");
-                
+
                 int number1 = input2 - input1;
                 Console.WriteLine("The difference is " + number1);
 
@@ -218,7 +247,7 @@ namespace Exercise_1
 
                 int number3 = input2 * input1;
                 Console.WriteLine("The product is " + number3);
-                
+
                 int number4 = input2 / input1;
                 Console.WriteLine("The ratio is " + number4);
             }
@@ -300,7 +329,7 @@ namespace Exercise_1
 
             if (userAge >= 18)
             {
-                OrderBeer();   
+                OrderBeer();
             }
 
             else if (userAge < 18)
@@ -382,7 +411,7 @@ namespace Exercise_1
                 try
                 {
 
-                    
+
                     Console.WriteLine("Choose between option 1, 2, or 3, or -1 if you want to go back");
                     var optionChoice = int.Parse(Console.ReadLine() ?? "");
                     switch (optionChoice)
@@ -450,7 +479,7 @@ namespace Exercise_1
 
             static void ColorChange()
             {
-                
+
                 if (Console.ForegroundColor == ConsoleColor.Green)
                 {
                     Console.ForegroundColor = ConsoleColor.Magenta;
@@ -463,9 +492,9 @@ namespace Exercise_1
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
                 }
-                
+
             }
-   
+
 
         }
 
@@ -474,18 +503,48 @@ namespace Exercise_1
             int userInput;
             Console.WriteLine("Please enter a number: ");
             userInput = TextToNum(Console.ReadLine());
-            
+
+
             if (userInput <= 0)
             {
                 Console.WriteLine("I'm sorry, you have to enter a number greater than 0");
                 Console.ReadKey();
                 Console.Clear();
                 RunExerciseEleven();
+
             }
-
-            for (int i = 0; i < 5; i++ )
+            else
             {
+                Console.Clear();
+                for (int k = 0; k <= userInput; k++)
+                {
+                    if (k % 2 == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
 
+                    Console.WriteLine(k);
+                }
+
+                Console.WriteLine("----------------------------------------");
+
+                for (int f = userInput; f >= 0; f--)
+                {
+                    if (f % 2 == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+
+                    Console.WriteLine(f);
+                }
             }
 
             static int TextToNum(string textNum)
@@ -499,5 +558,146 @@ namespace Exercise_1
             }
         }
 
+        private static void RunExerciseTwelve()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Multiplication table for " + i);
+                Console.ResetColor();
+
+                for (int k = 1; k <= 10; k++)
+                {
+                    Console.WriteLine(k * i);
+                }
+
+                Console.WriteLine("\t");
+            }
+
+        }
+
+        private static void RunExerciseThirteen()
+        {
+            Random random = new Random();
+            int hiddenNum = random.Next(1, 501);
+            bool keepLooping = true;
+
+            Console.Clear();
+            while (keepLooping == true)
+            {
+                Console.WriteLine("Try to guess what number I'm thinking of.\nPlease enter a number:");
+                int userInput = TextToNum(Console.ReadLine());
+
+                if (userInput == hiddenNum)
+                {
+                    Console.WriteLine("Congratulations, it was the right number!");
+
+                    keepLooping = false;
+                }
+                else if (userInput > hiddenNum)
+                {
+                    Console.WriteLine("Your guess was too big, please try again");
+                    Console.WriteLine("Press any key:");
+                    Console.ReadKey(true);
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.WriteLine("Your guess was too small, please try again");
+                    Console.WriteLine("Press any key:");
+                    Console.ReadKey(true);
+                    Console.Clear();
+                }
+            }
+
+
+
+            static int TextToNum(string textNum)
+            {
+                int num = 0;
+
+                int.TryParse(textNum, out num);
+
+                return num;
+
+            }
+        }
+
+        private static void RunExerciseFourteen()// not done.
+        {
+            bool keeplooping = true;
+            while (keeplooping == true)
+            {
+
+                Console.Write("Please enter a number: ");
+                string userInput = Console.ReadLine();
+                List<int> list = new List<int>(10);
+                int userNum;
+                int.TryParse(userInput, out userNum);
+
+
+                if (userNum == -1)
+                {
+                    int sum = list.Sum();
+                    Console.WriteLine("Sum: " + sum);
+                    keeplooping = false;
+                }
+                else
+                {
+                    list.Add(userNum);
+                }
+
+            }
+
+        }
+
+        private static void RunExerciseFifteen()
+        {
+
+        }
+
+        private static void RunExerciseSixteen()
+        {
+
+        }
+
+        private static void RunExerciseSeventeen()
+        {
+
+        }
+
+        private static void RunExerciseEightteen()
+        {
+            int[] array = new int[10];
+            double[] doubleArray = new double[10];
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 101);
+                doubleArray[i] = 1.0 / array[i];
+            }
+          
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(" ");
+
+            foreach (var item in doubleArray)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+
+
+        private static void Writeline(string text)
+        {
+            Console.WriteLine(text);
+        }
     }
 }
+
